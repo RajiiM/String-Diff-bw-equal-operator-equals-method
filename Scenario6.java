061c5c7ca2c2c9d85c7aa8fc3dd57dfb743fe53a
RajiiM <mrajalakshmiofficial@gmail.com>
2021-12-03 10:19:10 +0530

    FirstCommit

---
+++ Scenario6.java
@@ -0,0 +1,33 @@
+//Output
+Not ==
+equals
+//
+
+public class Sample 
+{
+	public static void main(String[] args) 
+	{
+		String s1 = new String("Java");
+		String s2 = s1;
+		s1 = new String("Python");
+		s2 = new String("Python");
+		
+		if(s1==s2)
+		{
+			System.out.println("==");
+		}
+		else
+		{
+			System.out.println("Not ==");
+		}
+		
+		if(s1.equals(s2))
+		{
+			System.out.println("equals");
+		}
+		else
+		{
+			System.out.println("Not equals");
+		}
+	}
+}
