f098903bbb62fb1f74828cd072a1a5a87aa35569
RajiiM <mrajalakshmiofficial@gmail.com>
2021-12-03 10:17:30 +0530

    FirstCommit

---
+++ Scenario2.java
@@ -0,0 +1,31 @@
+//Output
+==
+equals
+//
+
+public class Sample 
+{
+	public static void main(String[] args) 
+	{
+		String s1 = new String("Java");
+		String s2 = s1;
+		
+		if(s1==s2)
+		{
+			System.out.println("==");
+		}
+		else
+		{
+			System.out.println("Not ==");
+		}
+		
+		if(s1.equals(s2))
+		{
+			System.out.println("equals");
+		}
+		else
+		{
+			System.out.println("Not equals");
+		}
+	}
+}
